5fab95ff2a4ccddc8d41d33b4f74d245e6193b7c
Mohshin Ahmed <mohshina1@outlook.com>
2022-06-30 15:11:11 +0100

    Simplify part 1 solution

--- Day-01/day_01.py
+++ Day-01/day_01.py
@@ -3,9 +3,9 @@ with open('input.txt', 'r') as f:
 
 nums = [int(line) for line in input.splitlines()]
 
-count = 0
-for i in range(1, len(nums)):
-    if nums[i] > nums[i - 1]:
-        count += 1
+count = sum(
+    nums[i] > nums[i - 1]
+    for i in range(1, len(nums))
+)
 
 print(count)

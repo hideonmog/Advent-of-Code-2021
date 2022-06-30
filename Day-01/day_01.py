a84c2b1ff09bad75642435233c4205cdcc116432
Mohshin Ahmed <mohshina1@outlook.com>
2022-06-30 15:25:26 +0100

    Update day_01.py

--- Day-01/day_01.py
+++ Day-01/day_01.py
@@ -5,18 +5,18 @@ with open('input.txt', 'r') as f:
 
 nums = [int(line) for line in input.splitlines()]
 
-count = sum(
+count1 = sum(
     nums[i] > nums[i - 1]
     for i in range(1, len(nums))
 )
 
-print(f'Count for part 1: {count}')
+print(f'Count for part 1: {count1}')
 
 # part 2
 
-count = sum(
+count2 = sum(
     nums[i] > nums[i - 3]
     for i in range(3, len(nums))
 )
 
-print(f'Count for part 1: {count}')
+print(f'Count for part 1: {count2}')

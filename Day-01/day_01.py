763ed4ab144160aa00bcc026bbef62e1ee76c3b2
Mohshin Ahmed <mohshina1@outlook.com>
2022-06-30 15:23:52 +0100

    Complete part 2 using simplified part 1 solution

--- Day-01/day_01.py
+++ Day-01/day_01.py
@@ -1,6 +1,8 @@
 with open('input.txt', 'r') as f:
     input = f.read()
 
+# part 1
+
 nums = [int(line) for line in input.splitlines()]
 
 count = sum(
@@ -8,4 +10,13 @@ count = sum(
     for i in range(1, len(nums))
 )
 
-print(count)
+print(f'Count for part 1: {count}')
+
+# part 2
+
+count = sum(
+    nums[i] > nums[i - 3]
+    for i in range(3, len(nums))
+)
+
+print(f'Count for part 1: {count}')

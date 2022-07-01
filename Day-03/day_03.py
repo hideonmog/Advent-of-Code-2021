3123f70176a73e31ca69a8ccdcecb530b430b05a
Mohshin Ahmed <mohshina1@outlook.com>
2022-07-01 01:11:34 +0100

    Complete part 2

--- Day-03/day_03.py
+++ Day-03/day_03.py
@@ -1,3 +1,6 @@
+from unittest.mock import NonCallableMagicMock
+
+
 with open('input.txt', 'r') as f:
     input = f.read().splitlines()
 
@@ -22,3 +25,51 @@ for i in range(len(input[0])):
         epsilon += 1
 
 print(f'The power consumption of the submarine is: {gamma_rate*epsilon}')
+
+# part 2
+
+input2 = input.copy()
+index = 0
+while len(input2) !=1:
+    one = 0
+    zero = 0
+    ones = []
+    zeros = []
+    for line in input2:
+        if line[index] == '0':
+            zero += 1
+            zeros.append(line)
+        else:
+            one += 1
+            ones.append(line)
+    if zero > one:
+        input2 = zeros
+    else:
+        input2 = ones
+    index += 1
+
+oxygen = int(''.join(input2), 2)
+
+input2 = input.copy()
+index = 0
+while len(input2) !=1:
+    one = 0
+    zero = 0
+    ones = []
+    zeros = []
+    for line in input2:
+        if line[index] == '0':
+            zero += 1
+            zeros.append(line)
+        else:
+            one += 1
+            ones.append(line)
+    if zero > one:
+        input2 = ones
+    else:
+        input2 = zeros
+    index += 1
+
+co2 = int("".join(input2), 2)
+
+print(f'The life support rating of the submarine is: {oxygen*co2}')
